5508764be21934781c415df427ef97730bf99395
Adam Gerber <adam.gerber@yahoo.com>
2013-09-26 16:12:07 -0500

    minor edit to Driver comment text

--- src/P0_3/Driver.java
+++ src/P0_3/Driver.java
@@ -2,7 +2,7 @@ package P0_3;
 
 /**
  Name your packages according to its Programming Exercise identifier, except replace the period with underscore.
- For example, if the Programming Exercise identifier is P1.15, then name your package file P1_15
+ For example, if the Programming Exercise identifier is P1.15, then name your package P1_15
  */
 public class Driver {
 
