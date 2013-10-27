3a0227a5a67ee5119dfcc5071da0a09a5464e8f3
Eric Meschke <ericmeschke@gmail.com>
2013-10-27 13:51:52 -0500

    Final changes

--- src/P9_23/Driver.java
+++ src/P9_23/Driver.java
@@ -1,5 +1,6 @@
 package P9_23;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -11,9 +12,17 @@ import java.util.Scanner;
  * Date: 10/25/13
  * Time: 8:00 PM
  * To change this template use File | Settings | File Templates.
+ //This question was really out of control, it is way to much mashed together without good explanation of what is
+ required.  This has many methods and uses the four requested classes.  The classes are Appointment, and then sub-classes
+ of daily, monthly and onetime.  The main method initializes an array of Appointments and then populates it with three
+ example appointments.  It then calls a menu method and runs through an if statement while the menu choice is valid.
+ Otherwise the methods below implement the required menu options with a couple of extra methods to validate input.
+
+ This one was frustrating because of all the input and edge cases that could mess up the program coming from user input.
+ Is there a more succinct way to use the classes in java to validate input and arrange a menu class like this?
  */
 public class Driver {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Array list for the values of different appointments.
         ArrayList<Appointment> aAppointments = new ArrayList<Appointment>();
         //Initialize a few objects of different types to populate the array.
@@ -40,11 +49,11 @@ public class Driver {
                 choice = menu();
             }
             else if(choice == 4){
-                //save method
+                writeToFile(aAppointments);
                 choice = menu();
             }
             else if(choice ==5){
-                //load method
+                reader(aAppointments);
                 choice = menu();
             }
         }
@@ -60,7 +69,7 @@ public class Driver {
         System.out.println("1. Print list of appointments.");
         System.out.println("2. Add an appointment.");
         System.out.println("3. Check the calendar.");
-        System.out.println("4. Save an appointment to file.");
+        System.out.println("4. Save an appointment calendar to file.");
         System.out.println("5. Load an appointment from a file.");
         System.out.print("Any other key to exit. Choice:");
         Scanner in =new Scanner(System.in);
@@ -81,6 +90,7 @@ public class Driver {
     }
 
     public static void addAppt(ArrayList<Appointment> aList){
+        //Add an appointment to the list after checking that inputs are valid an ints.
         String type;
         String desc;
         String sDate;
@@ -216,5 +226,80 @@ public class Driver {
 
         }
     }
+
+    public static void writeToFile(ArrayList<Appointment> aList){
+
+        //Try catch to write the data to the file.
+        try {
+            //Write the appointment calendar to file using PrintWriter.
+            PrintWriter out = new PrintWriter("appointments.txt");
+
+            //Use a for loop to loop through and print to the file.
+            for (int i = 0; i < aList.size() ; i++) {
+                out.println(aList.get(i).getsDesc() + "," + aList.get(i).getIntDate() + "," + aList.get(i).getIntMonth() +
+                        "," + aList.get(i).getIntYear() + "\n");
+            }
+
+            out.close();
+
+            System.out.println("Write to file successful!");
+
+        } catch (IOException e) {
+            System.out.println("Not successfully written.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void reader(ArrayList<Appointment> aList) throws IOException{
+        //Method loads from a file that is in the same place as where the previous method writes to.  The format
+        //of the file to load is string,int,int,int where the ints are date, month, year.  Included is appointments1.txt
+        //Please direct the program to the appropriate path to load it.
+        try{
+            //Figure out what type of appointment to create.
+            String apptType;
+            String strInputFile;
+            String apptInfo;
+            System.out.print("Enter type of appointment to create Daily (d), Monthly (m), One-time (o):");
+            Scanner in = new Scanner(System.in);
+            apptType = in.nextLine().toLowerCase();
+            //Get file name, open file.
+            System.out.println("Enter a file from which to input data:");
+            strInputFile = in.nextLine();
+            File inputFile = new File(strInputFile);
+            Scanner scFile = new Scanner(inputFile);
+            //Read in first line of file and split it into pieces based on commas.
+            apptInfo = scFile.nextLine();
+            //System.out.println(apptInfo);
+            String[] splitStr = apptInfo.split(",");
+            //Assign the appointment to the file based on type specified.
+            System.out.println(apptType);
+            if (apptType.equals("d")){
+                Daily newD = new Daily(splitStr[0], Integer.parseInt(splitStr[1]), Integer.parseInt(splitStr[2]),
+                                       Integer.parseInt(splitStr[3]));
+                aList.add(newD);
+            }
+            else if (apptType.equals("m")){
+                Monthly newD = new Monthly(splitStr[0], Integer.parseInt(splitStr[1]), Integer.parseInt(splitStr[2]),
+                        Integer.parseInt(splitStr[3]));
+                aList.add(newD);
+            }
+            else if (apptType.equals("o")){
+                Onetime newD = new Onetime(splitStr[0], Integer.parseInt(splitStr[1]), Integer.parseInt(splitStr[2]),
+                        Integer.parseInt(splitStr[3]));
+                aList.add(newD);
+            }
+            else{
+                System.out.println("Appointment type was not valid.");
+            }
+            scFile.close();
+        }
+
+        catch (IOException e){
+            System.out.println("Couldn't read from file.");
+        }
+
+    }
+
+
 }
 
